a5038581a4e0f01b08ba1565b6c83669b4438178
Joseph Ugarte <jeugarte14@gmail.com>
2023-05-21 19:56:56 -0400

    Cleaned up code and comments

--- transcribe.js
+++ transcribe.js
@@ -16,8 +16,6 @@ function transcribe() {
 
   for (var i = 0; i < words.length; i++) {
     var word = words[i];
-    console.log(word);
-
     var num_syllables = 0
     for (var j = 0; j < word.length; j+=2) {
       var has_sh = false;
@@ -46,12 +44,10 @@ function transcribe() {
       }
       
     }
-    console.log(num_syllables);
 
     var rowSize = Math.ceil(num_syllables / 2);
 
     if (x + (Math.ceil(num_syllables/2) * 40) > lineWidth) {
-      // Move to the next line
       x = 50;
       y += 150;
     }
@@ -74,19 +70,12 @@ function transcribe() {
       }
       var shapes = syllableMapping[syllable_string];
 
-      
-
-
       var xPos = x + (counter % rowSize) * syllableSpacing;
       var yPos = y + (counter < rowSize ? -1 : 1) * Math.floor(counter / rowSize) * syllableSpacing;
 
       for (var k = 0; k < shapes.length; k++) {
         var shape = shapes[k];
-
-        drawCharacter(context, xPos, yPos, shape.shape, shape);
-
-        //xPos += shape.shape === "circle" ? shape.radius * 2 : shape.length;
-        
+        drawCharacter(context, xPos, yPos, shape.shape, shape);        
       }
 
       if (has_sh) {
@@ -117,54 +106,12 @@ function transcribe() {
         }
         j++;
       }
-
-      
       xPos += syllableSpacing;
       counter += 1;
-
-      
     }
 
     x = xPos + wordSpacing;
   }
-    // console.log("dog1");
-    // var canvas = document.getElementById("canvas");
-    // var context = canvas.getContext("2d");
-    // var inputText = document.getElementById("inputText").value;
-
-    // var x = 50;
-    // var y = canvas.height / 2;
-    // var syllableSpacing = 50; // Adjust as needed
-    // var wordSpacing = 100;
-    // console.log("dog2");
-    // for (var i = 0; i < inputText.length; i += 2) {
-    //     var syllable = inputText.substr(i, 2);
-
-    //     if (syllable.substr(0,1) === " ") {
-    //       x += wordSpacing;
-    //       i --;
-    //       continue;
-    //     }
-
-    //     if (syllable === "sh") {
-    //       syllable = inputText.substr(i, 3);
-    //       i++;
-    //     }
-
-
-    //     var shapes = syllableMapping[syllable];
-    //     console.log("dog3");
-
-    //     console.log(shapes.length)
-    //     for (var j = 0; j < shapes.length; j++) {
-    //         var shape = shapes[j];
-    //         console.log("dog4");
-    //         drawCharacter(context, x, y, shape.shape, shape);
-
-    //         //x += shape.shape === "circle" ? shape.radius * 2 : shape.length;
-    //     }
-    //     x += syllableSpacing;
-    // }
     var button = document.getElementById("transcribeButton");
     button.disabled = true;
     button.style.backgroundColor = "green";
@@ -211,11 +158,4 @@ function drawCharacter(context, x, y, shape, options) {
     }
   }
 
-function drawLine(context, sx, sy, ex, ey, options) {
-  context.beginPath();
-  context.moveTo(sx + options.startX, y + options.startY);
-  context.lineTo(ex + options.endX, y + options.endY);
-  context.stroke();
-}
-  
   
